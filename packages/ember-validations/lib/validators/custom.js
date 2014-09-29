a5ddc02f4c8e1638241792f89a8ff8db732425de
Paweł Sołtysiak <soltys@uznam.org>
2014-09-29 10:18:43 +0200

    added promise support for custom functions, removed dist catalog after moving to new branch

--- packages/ember-validations/lib/validators/custom.js
+++ packages/ember-validations/lib/validators/custom.js
@@ -7,7 +7,16 @@ Ember.Validations.validators.local.Custom = Ember.Validations.validators.Base.ex
     }
   },
   call: function() {
+    var self = this;
     var myValidator = this.options['myValidator'];
+    if (typeof myValidator.then === 'function' ) {
+      myValidator(this.model).then(function(isValid){
+        if (!isValid) {
+          self.errors.pushObject(self.options.message);
+        }
+      });
+      return;
+    }
     if (typeof myValidator === 'function' && !myValidator(this.model)) {
       this.errors.pushObject(this.options.message);
     }
