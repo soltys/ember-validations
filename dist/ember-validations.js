d719343dda5bf1541892cadda3ba9fa3496b56de
Paweł Sołtysiak <soltys@uznam.org>
2014-09-29 11:49:16 +0200

    hot fix for promise validation

--- dist/ember-validations.js
+++ dist/ember-validations.js
@@ -769,31 +769,41 @@ Ember.Validations.validators.local.Url = Ember.Validations.validators.Base.exten
 
 (function() {
 Ember.Validations.validators.local.Custom = Ember.Validations.validators.Base.extend({
-  init: function() {
-    this._super();
+    init: function() {
+        this._super();
 
-    if (this.options.message === undefined) {
-      this.set('options.message',  Ember.Validations.messages.render('invalid', this.options));
-    }
-  },
-  call: function() {
-    var self = this;
-    var myValidator = this.options['myValidator'];
-    if (typeof myValidator.then === 'function' ) {
-      myValidator(this.model).then(function(isValid){
-        if (!isValid) {
-          self.errors.pushObject(self.options.message);
+        if (this.options.message === undefined) {
+            this.set('options.message', Ember.Validations.messages.render('invalid', this.options));
         }
-      });
-      return;
+    },
+    to: true,
+    call: function() {
+        var self = this;
+        var throttleTime = self.options['throttleTime'] || 500;
+        throttle = function(func, delay) {
+            if (self.get('to')) {
+                window.clearTimeout(self.get('to'));
+            }
+            self.set('to', window.setTimeout(func, delay));
+        };
+
+        var promiseValidator = self.options['promiseValidator'];
+        if (typeof promiseValidator === 'function') {
+            throttle(function() {
+                promiseValidator(self.model).then(function(isValid) {
+                    if (!isValid) {
+                        self.errors.pushObject(self.options.message);
+                    }
+                });
+            }, throttleTime);
+            return;
+        }
+        var myValidator = this.options['myValidator'];
+        if (typeof myValidator === 'function' && !myValidator(this.model)) {
+            this.errors.pushObject(this.options.message);
+        }
     }
-    if (typeof myValidator === 'function' && !myValidator(this.model)) {
-      this.errors.pushObject(this.options.message);
-    }
-  }
 });
-
-
 })();
 
 
